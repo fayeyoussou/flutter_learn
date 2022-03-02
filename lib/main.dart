a9fe77a68fe62630eaae89e26118781835d944a5
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-03-02 11:13:02 +0000

    Basics/Dart_Basics

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+/**
+ *  main is a special function name
+ *  it is like java main though
+ *  EntryPoint of dart 
+ *  dart will automatically look for main file 
+ *  and then look for a main function   
+ */
+void main () {
+
+}
