237e3bd4036a7c3a197758bb15a8a963f866b452
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-03-02 11:20:40 +0000

    Basics/Variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+double addNumber(double num1, double num2) {
+  return num1 + num2;
+}
+
 /**
  *  main is a special function name
  *  it is like java main though
@@ -5,6 +9,4 @@
  *  dart will automatically look for main file 
  *  and then look for a main function   
  */
-void main () {
-
-}
+void main() {}
