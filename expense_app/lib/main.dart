6e7f1444aea76384ebc8ed515f076b7267da8e97
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-03-05 19:33:43 +0000

    04_Widget/04_transactionList

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,8 +1,10 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,37 +15,39 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: 'T1', title: 'Chaussure', amount: 1000, date: DateTime.now()),
+    Transaction(id: 'T2', title: 'Costume', amount: 2000, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-        Container(
-          width: double.infinity,
-          height: 100,
-          child: Card(
-            color: Color.fromARGB(255, 209, 38, 104),
-            child: Text('Chart!'),
-            elevation: 5,
-            
-            
-          ),
+        appBar: AppBar(
+          title: Text('Flutter App'),
         ),
-        Container(
-          
-          height: 50,
-          child: Card(
-            color: Colors.tealAccent,
-            child: Text('List of transaction'),
-            elevation: 10,
-          ),
-        )
-      ],)
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: Card(
+                color: Color.fromARGB(255, 209, 38, 104),
+                child: Text('Chart!'),
+                elevation: 5,
+              ),
+            ),
+            Container(
+              height: 50,
+              child: Card(
+                color: Colors.tealAccent,
+                child: Text('List of transaction'),
+                elevation: 10,
+              ),
+            )
+          ],
+        ));
   }
 }
