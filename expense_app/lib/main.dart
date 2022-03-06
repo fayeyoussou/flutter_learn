3eb339bd273592dcbdcda5618a3d1092fb5d7791
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-03-06 11:16:09 +0000

    04_Widget/09_inputShow

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,5 +1,7 @@
 import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,23 +22,61 @@ class MyHomePage extends StatelessWidget {
         id: 'T1', title: 'Chaussure', amount: 1000, date: DateTime.now()),
     Transaction(id: 'T2', title: 'Costume', amount: 2000, date: DateTime.now())
   ];
+  // String? titleInput;
+  // String? amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    initializeDateFormatting();
+    // Intl.defaultLocale = 'fr_FR';
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // it is the default setting
+          //mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
               width: double.infinity,
-              height: 100,
+              height: 50,
               child: Card(
                 color: Color.fromARGB(255, 209, 38, 104),
                 child: Text('Chart!'),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: titleController,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        controller: amountController,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          print(amountController.text);
+                          print(titleController.text);
+                        },
+                        child: Text('Valider'),
+                        style: TextButton.styleFrom(
+                            primary: Colors.deepPurple,
+                            textStyle: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15)),
+                      )
+                    ]),
               ),
             ),
             Column(
@@ -51,36 +91,33 @@ class MyHomePage extends StatelessWidget {
                                   vertical: 10, horizontal: 15),
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(color: Colors.purple,width: 3),
-                                  color: Colors.cyan
-                                  
-                                  ),
+                                  border: Border.all(
+                                      color: Colors.purple, width: 3),
+                                  color: Colors.cyan),
                               child: Text(
-                                e.amount.toString(),
-                                style:TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple
-                                ),),
+                                '${e.amount.toInt()} F',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: Colors.purple),
+                              ),
                             ),
                             Column(
-                              
-                              crossAxisAlignment: CrossAxisAlignment.end ,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  e.title
-                                  ,style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15,
-                                    color: Colors.amber
-                                  ),),
-                                Text(
-                                  e.date.toString(),
+                                  e.title,
                                   style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 10
-                                  ),
-                                  )
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.amber),
+                                ),
+                                Text(
+                                  DateFormat('EEEEE d MMM yyyy', 'fr')
+                                      .format(e.date),
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 10),
+                                )
                               ],
                             )
                           ],
